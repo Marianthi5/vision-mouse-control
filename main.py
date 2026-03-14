cbb079e717359ed833ecdc0041fc1a0214c233e2
Maro <mich.marianthi@gmail.com>
2026-03-14 15:13:51 +0200

    Isolate index finger and track its pixel coordinates

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ mp_draw = mp.solutions.drawing_utils
 def main():
     # Initialize camera
     cap = cv2.VideoCapture(0)
-    
+
     while True:
         success, frame = cap.read()
         if not success:
@@ -26,10 +26,19 @@ def main():
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Process the frame to find hands
         result = hands.process(rgb_frame)
-        # If a hand is detected, draw the landmarks
+        # If a hand is detected, process the landmarks
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
+                # Draw the basic web
                 mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                # Get the height and width of camera frame
+                h, w, c = frame.shape
+                # Isolate Landmark 8 (Index Finger Tip)
+                index_finger = hand_landmarks.landmark[8]
+                # Convert the normalized ratio (0.0 - 1.0) into pixel coordinates
+                cx, cy = int(index_finger.x * w), int(index_finger.y * h)
+                cv2.circle(frame, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+                print(f"Index Finger Tip - X: {cx}, Y: {cy}")
         cv2.imshow("Vision Mouse Control", frame)
         # Press esc to quit
         if cv2.waitKey(1) & 0xFF == 27:
